b17e988014dda305e50bd157d41d234adfdd88a3
pacmadhushan <chathuramadhushanpa@gmail.com>
2023-01-21 23:34:01 +0530

    initial comit

---
+++ Spring_rest_api_project/src/main/java/lk/ijse/dep9/Spring_rest_api_project/SpringRestApiProjectApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep9.Spring_rest_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiProjectApplication.class, args);
+	}
+
+}
---
+++ Spring_rest_api_project/src/test/java/lk/ijse/dep9/Spring_rest_api_project/SpringRestApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep9.Spring_rest_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
